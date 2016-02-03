ab412ee3050115f042f58e12bc038a3d987be354
Brett Menzies <scimor5@gmail.com>
2016-02-02 21:17:53 -0800

    documentation additions

--- src/util.hpp
+++ src/util.hpp
@@ -2,18 +2,22 @@
 #include <cmath>
 #include <functional>
 
-double step(double v){
+typedef std::function<double(double)> Decider;
+
+inline double step(double v){
     return (v < 0.0)? 0.0 : 1.0;
 }
 
-std::function<double(double)> stepFunc(double cutoff){
-    return [cutoff](double v) -> double { return step(v+cutoff); };
-}
-
-double sigmoid(double v){
+inline double sigmoid(double v){
     return 1.0 / (1.0 + exp(-v));
 }
 
+/** Return a step function that switches from 0.0 to 1.0 at `cutoff` */
+std::function<double(double)> stepFunc(double cutoff){
+    return [cutoff](double v) -> double { return step(v-cutoff); };
+}
+
+/** Return a sigmoid function with slope `slope` at 0.0 */
 std::function<double(double)> sigmoidFunc(double slope){
     return [slope](double v) -> double { return sigmoid(v*slope); };
 }
