3cbd857dffc9b874f0dc5ee126f622122966fc33
Brett Menzies <scimor5@gmail.com>
2016-02-03 11:02:44 -0800

    Added method stubs to Matrix class

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -1,5 +1,23 @@
 #pragma once
+#include <functional>
 
 class Matrix{
-
+public:
+    const int rows;
+    const int columns;
+    Matrix(int rows, int columns);
+    Matrix T(); //transpose
+    void fill(double value);
+    double get(int row, int column);
+    void set(int row, int column, double value);
+    void map(std::function<double(double)> transform);
+    Matrix& operator+= (const Matrix& rhs);
+    Matrix  operator- (const Matrix& rhs) const;
+    Matrix  operator+ (const Matrix& rhs) const;
+    Matrix  operator* (const Matrix& rhs) const;
+    Matrix  operator* (double scalar) const;
+    Matrix  operator| (const Matrix& rhs) const;
+    friend Matrix operator* (double scalar, const Matrix& rhs);
 };
+
+
