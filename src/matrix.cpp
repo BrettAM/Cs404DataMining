7c865a7235206c8fab21587860e648c4513d4608
Brett Menzies <scimor5@gmail.com>
2016-02-03 21:17:14 -0800

    Untested first draft matrix library code

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,1 +1,111 @@
 #include "matrix.hpp"
+
+Matrix::Matrix(int rows, int columns)
+    : rows(rows), cols(columns) {
+    data = new double[rows*cols];
+}
+Matrix::~Matrix(){
+    delete[] data;
+}
+Matrix::Matrix(const Matrix& other)
+    : rows(other.rows), cols(other.cols) {
+    data = new double[rows*cols];
+    std::copy(other.data, other.data+rows*cols, data);
+}
+//copy constructor
+Matrix Matrix::T(){ //transpos
+    Matrix nm(cols, rows);
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < cols; c++){
+            nm.set(c, r, get(r,c));
+        }
+    }
+    return nm;
+}
+void Matrix::fill(double value){
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < cols; c++){
+            set(r, c, value);
+        }
+    }
+}
+void Matrix::map(std::function<double(double)> transform){
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < cols; c++){
+            double value = transform(get(r,c));
+            set(r, c, value);
+        }
+    }
+}
+Matrix& Matrix::operator+= (const Matrix& rhs){
+    assert (this->rows == rhs.rows);
+    assert (this->cols == rhs.cols);
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < cols; c++){
+            double value = this->get(r,c) + rhs.get(r,c);
+            set(r, c, value);
+        }
+    }
+    return *this;
+}
+Matrix  Matrix::operator- (const Matrix& rhs) const{
+    assert (this->rows == rhs.rows);
+    assert (this->cols == rhs.cols);
+    Matrix nm(rows, cols);
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < cols; c++){
+            double value = this->get(r,c) - rhs.get(r,c);
+            nm.set(r, c, value);
+        }
+    }
+    return nm;
+}
+Matrix  Matrix::operator+ (const Matrix& rhs) const{
+    assert (this->rows == rhs.rows);
+    assert (this->cols == rhs.cols);
+    Matrix nm(rows, cols);
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < cols; c++){
+            double value = this->get(r,c) + rhs.get(r,c);
+            nm.set(r, c, value);
+        }
+    }
+    return nm;
+}
+Matrix  Matrix::operator* (const Matrix& rhs) const{
+    assert (this->cols == rhs.rows);
+    Matrix nm(rows, rhs.cols);
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < rhs.cols; c++){
+            double value = 0.0;
+            for(int i=0; i<cols; i++){
+                value += get(r, i) * rhs.get(i, c);
+            }
+            nm.set(r, c, value);
+        }
+    }
+    return nm;
+}
+Matrix  Matrix::operator| (const Matrix& rhs) const{
+    assert (this->rows == rhs.rows);
+    Matrix nm(rows, cols);
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < cols; c++){
+            nm.set(r, c, this->get(r,c));
+        }
+    }
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < rhs.cols; c++){
+            nm.set(r, c+cols, rhs.get(r,c));
+        }
+    }
+    return nm;
+}
+Matrix operator* (double scalar, const Matrix& rhs){
+    Matrix nm = rhs;
+    nm.map([=](double v) -> double { return scalar*v; });
+    return nm;
+}
+Matrix operator* (const Matrix& rhs, double scalar){
+    return scalar*rhs;
+}
--- src/matrix.hpp
+++ src/matrix.hpp
@@ -1,23 +1,51 @@
 #pragma once
 #include <functional>
+#include <assert.h>
 
 class Matrix{
+private:
+    double* data; //rows by columns
 public:
     const int rows;
-    const int columns;
+    const int cols;
     Matrix(int rows, int columns);
-    Matrix T(); //transpose
+    ~Matrix();
+    Matrix(const Matrix& other);
+    /** return the value at `row` and `column` indexed at 0
+      * defined here for inlining
+      */
+    double get(int row, int column) const {
+        assert(row < rows);
+        assert(column < cols);
+        return data[row*cols+column];
+    }
+    /** set the value at `row` and `column` indexed at 0
+      * defined here for inlining
+      */
+    void set(int row, int column, double value){
+        assert(row < rows);
+        assert(column < cols);
+        data[row*cols+column] = value;
+    }
+    /** return a Matrix that is the transpose of this one */
+    Matrix T();
+    /** fill every element with `value` */
     void fill(double value);
-    double get(int row, int column);
-    void set(int row, int column, double value);
+    /** set every element to the result of transform(original_element) */
     void map(std::function<double(double)> transform);
+    /** add correspending elements in `rhs` to this matrix */
     Matrix& operator+= (const Matrix& rhs);
+    /** construct a matrix that is the elementwise difference of this and rhs */
     Matrix  operator- (const Matrix& rhs) const;
+    /** construct a matrix that is the elementwise sum of this and rhs */
     Matrix  operator+ (const Matrix& rhs) const;
+    /** construct a matrix that is the prodect this and rhs */
     Matrix  operator* (const Matrix& rhs) const;
-    Matrix  operator* (double scalar) const;
+    /** construct a matrix of this and `rhs` concatenated left to right */
     Matrix  operator| (const Matrix& rhs) const;
+    /** construct a matrix that is the elementwise product of this and scalar */
     friend Matrix operator* (double scalar, const Matrix& rhs);
+    friend Matrix operator* (const Matrix& rhs, double scalar);
 };
 
 
