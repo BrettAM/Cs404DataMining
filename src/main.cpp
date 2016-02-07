26895f4bbd2ffdf32a9f871106e5f3ec58e04066
Brett Menzies <scimor5@gmail.com>
2016-02-06 16:45:48 -0800

    Simple perceptron problem runner

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <fstream>
 #include "matrix.hpp"
+#include "PtronLayer.hpp"
+#include "util.hpp"
+#include "Input.hpp"
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-    Matrix matrix;
-    cout<<"Hello World\n";
+    if(argc < 2){
+        cerr << "Provide input file as the first argument";
+        exit(1);
+    }
+
+    ProblemSet ps = readProblem(argv[1]);
+    PtronLayer p(ps.inputCount, ps.outputCount, 0.25, stepFunc(0.5));
+
+    //cout << ps.trainingInput << "\n" << ps.trainingOutput << "\n" << ps.challenge;
+
+    for(int i=0; i<100; i++){
+        p.update(ps.trainingInput, ps.trainingOutput);
+        //if(i % 4 == 0) cout << p.getWeights().T() << "\n";
+    }
+
+    cout << p.apply(ps.challenge);
+
     return 0;
 }
